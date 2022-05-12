565c4d9abff86f1a124c10a361b568bd8d846998
DrummerHead <drummerhead@gmail.com>
2022-05-12 17:56:23 +0200

    Add example output json file and function to turn it into nodes and links for network visualiation

--- src/features/graph/screens/Graph.tsx
+++ src/features/graph/screens/Graph.tsx
@@ -1,6 +1,9 @@
+import React, { useEffect } from 'react';
 import { ResponsiveNetwork } from '@nivo/network';
 import styled from 'styled-components';
 import dataMock from '../data/dataMock.json';
+import exampleOutput from '../data/exampleOutput.json';
+import dataToGraph from '../data/dataToGraph';
 
 const Vessel = styled.main`
   height: 100vh;
@@ -8,37 +11,48 @@ const Vessel = styled.main`
 
 // Working graph with mock data for now
 //
-const Graph = () => (
-  <Vessel>
-    <ResponsiveNetwork
-      data={dataMock}
-      margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      linkDistance={function (e) {
-        return e.distance;
-      }}
-      centeringStrength={0.3}
-      repulsivity={6}
-      nodeSize={function (n) {
-        return n.size;
-      }}
-      activeNodeSize={function (n) {
-        return 1.5 * n.size;
-      }}
-      nodeColor={function (e) {
-        return e.color;
-      }}
-      nodeBorderWidth={1}
-      nodeBorderColor={{
-        from: 'color',
-        modifiers: [['darker', 0.8]],
-      }}
-      linkThickness={function (n) {
-        return 2 + 2 * n.target.data.height;
-      }}
-      linkBlendMode="multiply"
-      motionConfig="wobbly"
-    />
-  </Vessel>
-);
+const Graph = () => {
+  const nodesAndLinks = dataToGraph(exampleOutput);
+
+  useEffect(() => {
+    console.group('Graph page');
+    console.log('nodesAndLinks');
+    console.dir(nodesAndLinks);
+    console.groupEnd();
+  }, [nodesAndLinks]);
+
+  return (
+    <Vessel>
+      <ResponsiveNetwork
+        data={dataMock}
+        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        linkDistance={function (e) {
+          return e.distance;
+        }}
+        centeringStrength={0.3}
+        repulsivity={6}
+        nodeSize={function (n) {
+          return n.size;
+        }}
+        activeNodeSize={function (n) {
+          return 1.5 * n.size;
+        }}
+        nodeColor={function (e) {
+          return e.color;
+        }}
+        nodeBorderWidth={1}
+        nodeBorderColor={{
+          from: 'color',
+          modifiers: [['darker', 0.8]],
+        }}
+        linkThickness={function (n) {
+          return 2 + 2 * n.target.data.height;
+        }}
+        linkBlendMode="multiply"
+        motionConfig="wobbly"
+      />
+    </Vessel>
+  );
+};
 
 export default Graph;
