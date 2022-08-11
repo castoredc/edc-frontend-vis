45d1ef709521d32f3e812b85edb614f1a163e15e
Borys Koretskyi <borys.koretskyi@castoredc.com>
2022-08-11 11:11:51 +0200

    Remove unused functions

--- src/features/graph/data/dataToGraphCentral.ts
+++ src/features/graph/data/dataToGraphCentral.ts
@@ -1,4 +1,4 @@
-import { CJS, DataToGraphReturn, NetNode, NetLink, Node } from './types';
+import { DataToGraphReturn, Node } from './types';
 
 export const getInitialGraphData = (allNodes: Node[]): DataToGraphReturn =>
   allNodes.reduce<DataToGraphReturn>(
@@ -56,65 +56,3 @@ export const getInitialGraphData = (allNodes: Node[]): DataToGraphReturn =>
       links: [],
     }
   );
-
-export const getSecondaryNodesAndLinks = (
-  mainNodeName: string,
-  data: CJS
-): DataToGraphReturn => {
-  const bigArray = data.config;
-
-  const nodesAndLinks = bigArray.reduce<{ nodes: NetNode[]; links: NetLink[] }>(
-    (acc, current) => {
-      if (mainNodeName === current.name) {
-        const {
-          importedExtComponents,
-          importedReactComponents,
-          defaultExtComponents,
-        } = current.childComponents;
-
-        const allChild = [
-          ...importedExtComponents,
-          ...importedReactComponents,
-          ...defaultExtComponents,
-        ];
-
-        const nodes = allChild.map((child) => ({
-          id: `${mainNodeName}-${child.name}`,
-          height: 1,
-          color: 'rgb(232, 193, 160)',
-          size: 10,
-        }));
-
-        const links = allChild.map((importedName: any) => ({
-          source: mainNodeName,
-          target: `${mainNodeName}-${importedName.name}`,
-          distance: 130,
-        }));
-
-        return { nodes, links };
-      } else {
-        return { nodes: acc.nodes, links: acc.links };
-      }
-    },
-    { nodes: [], links: [] }
-  );
-
-  return nodesAndLinks;
-};
-
-export const removePreviousNodesAndLinks = (
-  mainNodeName: string,
-  data: CJS,
-  previousData: DataToGraphReturn
-): DataToGraphReturn => {
-  const nodeAndLinksToRemove = getSecondaryNodesAndLinks(mainNodeName, data);
-
-  return {
-    nodes: previousData.nodes.filter(
-      (ar) => !nodeAndLinksToRemove.nodes.find((rm) => rm.id === ar.id)
-    ),
-    links: previousData.links.filter(
-      (ar) => !nodeAndLinksToRemove.links.find((rm) => rm.source === ar.source)
-    ),
-  };
-};
